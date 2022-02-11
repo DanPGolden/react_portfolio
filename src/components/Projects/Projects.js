6d427c58d675da70a32ef3aa003a430d40758374
DanPGolden <mary_dangolden@comcast.net>
2022-02-11 13:42:00 -0800

    project update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,26 +4,30 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
-// const projectsDemo =
+ const projectsDemo = [{
+        title: 'Project 1',
+        description: 'This is a music application that users can search for albums, artists, and lyrics'
+      },
+      {
+        title: 'Project 2',
+        description: 'This hey now music application that users can search for albums, artists, and lyrics'
+      },
+      {
+        title: 'Project 3',
+        description: 'This is a music application that users can search for albums, artists, and lyrics'
+      },
+      {
+        title: 'Project 4',
+        description: 'This is a music application that users can search for albums, artists, and lyrics'
+      }]
 
 const Projects = () => (
   <Section no padding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {[{
-        title: 'Project 1',
-        description: 'This is a music application that users can search for albums, artists, and lyrics'
-      },
       {
-        title: 'Project 2',
-        description: 'This is a music application that users can search for albums, artists, and lyrics'
-      },
-      {
-        title: 'Project 3',
-        description: 'This is a music application that users can search for albums, artists, and lyrics'
-      }]
-      .map((project) => (
+      projectsDemo.map((project) => (
         <div>
           {project.title}
           <br />
