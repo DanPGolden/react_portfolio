6f7ce43a53619032971187b6d478043469d6ff7d
DanPGolden <mary_dangolden@comcast.net>
2022-02-11 13:33:57 -0800

    added projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,34 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+// const projectsDemo =
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section no padding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {[{
+        title: 'Project 1',
+        description: 'This is a music application that users can search for albums, artists, and lyrics'
+      },
+      {
+        title: 'Project 2',
+        description: 'This is a music application that users can search for albums, artists, and lyrics'
+      },
+      {
+        title: 'Project 3',
+        description: 'This is a music application that users can search for albums, artists, and lyrics'
+      }]
+      .map((project) => (
+        <div>
+          {project.title}
+          <br />
+          {project.description}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
