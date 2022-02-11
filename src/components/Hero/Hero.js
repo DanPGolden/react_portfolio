2147fdac9e4b4db7a9073bafddf2e6b7812aa7cc
DanPGolden <mary_dangolden@comcast.net>
2022-02-11 13:08:38 -0800

    hero layout structure

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I'm a full stack web developer based in Spokane, Washington. I design attractive, functional websites and can provide a total solution to my customers.
+      </SectionText>
+      <Button onClick={() => window.location = "https://www.google.com/" }>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
